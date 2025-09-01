f0ae315544cd257eb87507bb88bd298b986e4885
umutatras <umutatras@gmail.com>
2025-09-02 01:51:45 +0300

    Entityler oluşturuldu.

---
+++ MusteriBakiyeSeyri.Entities/Fatura/MusteriFatura.cs
@@ -0,0 +1,20 @@
+﻿using MusteriBakiyeSeyri.Entities.Musteri;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusteriBakiyeSeyri.Entities.Fatura;
+
+public class MusteriFatura : BaseEntity
+{
+    public int MusteriId { get; set; }
+    public DateTime FaturaTarihi { get; set; }
+    public decimal FaturaTutari { get; set; }
+    public DateTime OdemeTarihi { get; set; }
+
+    #region Navigation Property
+    public MusteriTanim  MusteriTanim { get; set; }
+    #endregion
+}
---
+++ MusteriBakiyeSeyri.Entities/Musteri/MusteriTanim.cs
@@ -0,0 +1,17 @@
+﻿using MusteriBakiyeSeyri.Entities.Fatura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusteriBakiyeSeyri.Entities.Musteri;
+
+public class MusteriTanim : BaseEntity
+{
+    public string Unvan { get; set; }
+
+    #region Navigation Property
+    public ICollection<MusteriFatura> MusteriFaturalari { get; set; } = new List<MusteriFatura>();
+    #endregion
+}
