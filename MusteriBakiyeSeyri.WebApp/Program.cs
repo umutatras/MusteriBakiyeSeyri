0ffde77e15dce1c6797944fc56c76f74bd5a0dfe
umutatras <umutatras@gmail.com>
2025-09-02 21:37:39 +0300

    dbscript

--- MusteriBakiyeSeyri.WebApp/Program.cs
+++ MusteriBakiyeSeyri.WebApp/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=MusteriFatura}/{action=Index}/{id?}");
 
 app.Run();
